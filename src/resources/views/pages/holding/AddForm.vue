16333ec565d8dcb00e721f131adbcbcbccf7d3ab
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2022-08-26 22:57:11 +0700

    fix

--- src/resources/views/pages/holding/AddForm.vue
+++ src/resources/views/pages/holding/AddForm.vue
@@ -5,10 +5,10 @@ form.row.row-cols-lg-auto.g-3.align-items-center(@submit.prevent="onSubmit" meth
         input#inputSymbol.form-control(v-model="symbol" type="text" placeholder="BTC" required)
     .col-12
         label.visually-hidden(for="inputAmount") Amount
-        input#inputAmount.form-control.text-end(v-model="amount" type="text" placeholder="0.00000000" required)
+        input#inputAmount.form-control.text-end(v-model="amount" @focus="onFocus" type="text" placeholder="0.00000000" required)
     .col-12
         label.visually-hidden(for="inputExchange") Exchange
-        select#inputExchange.form-select(v-model="exchange" @change="onExchangeChange")
+        select#inputExchange.form-select(v-model="exchange")
             option(v-for="value in exchanges" :key="value.id" :value="value.id" :selected="exchange === value.id")
                 | {{ value.name }}
     .col-12
@@ -50,6 +50,12 @@ export default {
                 this.exchange = this.exchanges.length ? this.exchanges[0].id : null
                 this.loading._ = false
             })
+        },
+        onFocus($event) {
+            const value = $event.target.value
+            if (/^0+(\.0+)?$/.test(value)) {
+                $event.target.setSelectionRange(0, value.length)
+            }
         },
         onSubmit() {
             this.$emit('add', {
